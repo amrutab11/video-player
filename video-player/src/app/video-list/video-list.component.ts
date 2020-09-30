2cf05715c0ce187cbb4bbdc12c61397644d5521f
amrutab11 <amrutabelgaonkar11998@gmail.com>
2020-09-30 17:03:28 +0530

    Input Output decorater

---
+++ video-player/src/app/video-list/video-list.component.ts
@@ -0,0 +1,27 @@
+import { Component, Input, OnInit ,EventEmitter, Output} from '@angular/core';
+import { Video } from '../video';
+
+@Component({
+  selector: 'video-list',
+  templateUrl: './video-list.component.html',
+  styleUrls: ['./video-list.component.css'],
+  //inputs:['videos']
+})
+export class VideoListComponent implements OnInit {
+
+  @Input()
+  videos;
+
+  @Output()
+  SelectVideo=new EventEmitter();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onselect(vid:Video){
+
+    this.SelectVideo.emit(vid);
+  }
+}
